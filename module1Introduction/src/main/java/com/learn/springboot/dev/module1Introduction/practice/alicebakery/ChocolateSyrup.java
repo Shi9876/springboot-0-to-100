73a975811bccf471fcee8262b9c2034e45e78e6a
Shi9876 <shivanshigusain.khati@gmail.com>
2026-08-16 11:39:25 +0530

    Add chocolate and strawberry syruo implementations

---
+++ module1Introduction/src/main/java/com/learn/springboot/dev/module1Introduction/practice/alicebakery/ChocolateSyrup.java
@@ -0,0 +1,8 @@
+package com.learn.springboot.dev.module1Introduction.practice.alicebakery;
+
+public class ChocolateSyrup implements Syrup{
+
+    public String getSyrupType(){
+        return "Chocolate";
+    }
+}
---
+++ module1Introduction/src/main/java/com/learn/springboot/dev/module1Introduction/practice/alicebakery/StrawberrySyrup.java
@@ -0,0 +1,8 @@
+package com.learn.springboot.dev.module1Introduction.practice.alicebakery;
+
+public class StrawberrySyrup {
+
+    public String getSyrupType(){
+        return "Strawberry";
+    }
+}
