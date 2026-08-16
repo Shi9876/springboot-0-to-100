44fa56dc0f298c3a2ecfa31234fab66c5aadcb08
Shi9876 <shivanshigusain.khati@gmail.com>
2026-08-16 11:07:34 +0530

    Add chocolate and strawberry frosting implemention

---
+++ module1Introduction/src/main/java/com/learn/springboot/dev/module1Introduction/practice/alicebakery/StrawberryFrosting.java
@@ -0,0 +1,9 @@
+package com.learn.springboot.dev.module1Introduction.practice.alicebakery;
+
+public class StrawberryFrosting implements Frosting{
+
+    @Override
+    public String getFrostingType() {
+        return "Strawberry";
+    }
+}
