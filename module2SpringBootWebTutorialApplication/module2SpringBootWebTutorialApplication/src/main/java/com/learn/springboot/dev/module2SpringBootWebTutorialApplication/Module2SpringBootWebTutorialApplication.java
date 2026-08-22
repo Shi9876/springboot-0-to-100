1e5416259da8ff4c3d2e1fd18696fdac1ecb0135
Shi9876 <shivanshigusain.khati@gmail.com>
2026-08-22 23:02:29 +0530

    Add Module 2 Spring Boot Web Tutorial

---
+++ module2SpringBootWebTutorialApplication/module2SpringBootWebTutorialApplication/src/main/java/com/learn/springboot/dev/module2SpringBootWebTutorialApplication/Module2SpringBootWebTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.learn.springboot.dev.module2SpringBootWebTutorialApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module2SpringBootWebTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module2SpringBootWebTutorialApplication.class, args);
+	}
+
+}
---
+++ module2SpringBootWebTutorialApplication/module2SpringBootWebTutorialApplication/src/test/java/com/learn/springboot/dev/module2SpringBootWebTutorialApplication/Module2SpringBootWebTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springboot.dev.module2SpringBootWebTutorialApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module2SpringBootWebTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
